745956d7c79a809eb05a8838c231f7e336e0b433
Soan Trinh <tmsoan@gmail.com>
2025-11-13 11:18:48 +0700

    Update app icon

--- app/src/main/java/com/anos/gitbrowse/MainActivity.kt
+++ app/src/main/java/com/anos/gitbrowse/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.anos.gitbrowse.navigation.AppNavHost
 import com.anos.ui.theme.GitBrowseColors
@@ -17,8 +18,9 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
             GitBrowseTheme {
